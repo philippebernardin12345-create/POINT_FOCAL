f46039e13321d2d30027f0a9ca3af4a8a9115747
philippebernardin12345-create <philippebernardin12345@gmail.com>
2026-06-29 16:59:22 +0100

    Update auth.routes.js

--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -1,1 +1,10 @@
-ok
+const express = require("express");
+const authController = require("./auth.controller");
+
+const router = express.Router();
+
+router.post("/register", authController.register);
+router.post("/login", authController.login);
+router.get("/confirm-email/:userId", authController.confirmEmail);
+
+module.exports = router;
