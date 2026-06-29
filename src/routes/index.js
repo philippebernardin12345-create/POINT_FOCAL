06c9252e607804ec3273c9d6163702bf25b77988
philippebernardin12345-create <philippebernardin12345@gmail.com>
2026-06-29 16:39:30 +0100

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
+
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.json({
-        status: "OK",
-        message: "API Point Focal V9"
-    });
+  res.json({
+    status: "OK",
+    message: "API Point Focal V9"
+  });
 });
 
 module.exports = router;
