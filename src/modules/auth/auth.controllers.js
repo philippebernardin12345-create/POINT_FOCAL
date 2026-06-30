b2e2fcdcd34e88fac4a3be83792a79da39ceb490
philippebernardin12345-create <philippebernardin12345@gmail.com>
2026-06-30 19:02:31 +0100

    Update auth.controllers.js

--- src/modules/auth/auth.controllers.js
+++ src/modules/auth/auth.controllers.js
@@ -1,45 +1,8 @@
-const authService = require("./auth.service");
-const response = require("../../core/response");
-
-async function register(req, res) {
-  try {
-    const result = await authService.register(req.body);
-    return response.success(res, result, result.message, 201);
-  } catch (err) {
-    return response.error(res, err.message, 400);
-  }
-}
-
-async function login(req, res) {
-  try {
-    const result = await authService.login(req.body);
-    return response.success(res, result, "Connexion réussie", 200);
-  } catch (err) {
-    return response.error(res, err.message, 401);
-  }
-}
-
-async function confirmEmail(req, res) {
-  try {
-    const result = await authService.confirmEmail(req.params.userId);
-    return response.success(res, result, "Email confirmé avec succès", 200);
-  } catch (err) {
-    return response.error(res, err.message, 400);
-  }
-}
-
-// Utilisateur connecté
 async function me(req, res) {
   try {
-    return response.success(res, req.user, "Utilisateur connecté", 200);
+    const result = await authService.me(req.user.id);
+    return response.success(res, result, "Utilisateur connecté", 200);
   } catch (err) {
     return response.error(res, err.message, 401);
   }
 }
-
-module.exports = {
-  register,
-  login,
-  confirmEmail,
-  me
-};
