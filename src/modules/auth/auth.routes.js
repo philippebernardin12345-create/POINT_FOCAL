f2548b1c394c52ab0c0c035632ff4cd29df2058e
philippebernardin12345-create <philippebernardin12345@gmail.com>
2026-06-30 18:38:59 +0100

    Update auth.routes.js

--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const authController = require("./auth.controllers");
+const authMiddleware = require("../../middlewares/auth.middleware");
 
 const router = express.Router();
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.get("/confirm-email/:userId", authController.confirmEmail);
+
+// Utilisateur connecté (protégé par JWT)
+router.get("/me", authMiddleware, authController.me);
 
 module.exports = router;
