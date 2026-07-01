eba86fef6effdcd820e5af98600105e7922611d9
philippebernardin12345-create <philippebernardin12345@gmail.com>
2026-07-01 12:21:52 +0100

    Update server.js

---
+++ src/server.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+
+const app = require("./app");
+
+const PORT = process.env.PORT || 5000;
+
+console.log("DATABASE_URL =", process.env.DATABASE_URL);
+
+app.listen(PORT, () => {
+  console.log(`Point Focal Backend V9 running on port ${PORT}`);
+});
