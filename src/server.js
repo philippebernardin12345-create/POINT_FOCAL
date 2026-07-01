f3a38db391e86bdd53f5b155d1ab69249b32bc0c
philippebernardin12345-create <philippebernardin12345@gmail.com>
2026-07-01 14:51:33 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,8 +4,6 @@ const app = require("./app");
 
 const PORT = process.env.PORT || 5000;
 
-console.log("DATABASE_URL =", process.env.DATABASE_URL);
-
 app.listen(PORT, () => {
   console.log(`Point Focal Backend V9 running on port ${PORT}`);
 });
