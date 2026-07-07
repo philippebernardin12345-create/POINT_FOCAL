3393343ab673c79946a6e58b9be3123409c284eb
philippebernardin12345-create <philippebernardin12345@gmail.com>
2026-07-07 21:49:11 +0100

    Update payments.routes.js

--- src/modules/payments/payments.routes.js
+++ src/modules/payments/payments.routes.js
@@ -1,1 +1,10 @@
 
+const express = require("express");
+const authMiddleware = require("../../middlewares/auth.middleware");
+const paymentsController = require("./payments.controller");
+
+const router = express.Router();
+
+router.post("/auto-trigger", authMiddleware, paymentsController.autoTrigger);
+
+module.exports = router;
