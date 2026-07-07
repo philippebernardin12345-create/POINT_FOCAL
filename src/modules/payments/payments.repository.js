e44c9c0d098ee4d3946643f0ff00572753b9e510
philippebernardin12345-create <philippebernardin12345@gmail.com>
2026-07-07 23:11:22 +0100

    Update payments.repository.js

--- src/modules/payments/payments.repository.js
+++ src/modules/payments/payments.repository.js
@@ -1,2 +1,17 @@
+const db = require("../../config/db");
 
-module.exports = {};
+async function findUserBySeries3Code(code) {
+  const result = await db.query(
+    `SELECT id, email, invitation_code_series_3
+     FROM users
+     WHERE invitation_code_series_3 = $1
+     LIMIT 1`,
+    [code]
+  );
+
+  return result.rows[0] || null;
+}
+
+module.exports = {
+  findUserBySeries3Code
+};
