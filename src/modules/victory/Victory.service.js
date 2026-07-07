31bb6a2ad8112ddca91daf6f72af018c9693676d
philippebernardin12345-create <philippebernardin12345@gmail.com>
2026-07-07 13:33:17 +0100

    Add Victory service with assignVictoryLink function

---
+++ src/modules/victory/Victory.service.js
@@ -0,0 +1,15 @@
+const DEFAULT_VICTORY_LINK = "https://victoryautomatic.com/user/register/okoningana':
+
+async function assignVictoryLink(userId) {
+  if (!userId) {
+    throw new Error("Utilisateur non authentifié.");
+  }
+
+  return {
+    url: DEFAULT_VICTORY_LINK
+  };
+}
+
+module.exports = {
+  assignVictoryLink
+};
