3b4b77ade9cf989f65301311f98c7c8e2d78e60e
philippebernardin12345-create <philippebernardin12345@gmail.com>
2026-07-07 12:48:00 +0100

    Update Victory service.js

--- src/modules/victory/Victory service.js
+++ src/modules/victory/Victory service.js
@@ -1,1 +1,15 @@
-ok
+const DEFAULT_VICTORY_LINK = "https://victoryautomatic.com/user/register/lobongo01";
+
+async function assignVictoryLink(userId) {
+  if (!userId) {
+    throw new Error("Utilisateur non authentifié.");
+  }
+
+  return {
+    url: DEFAULT_VICTORY_LINK
+  };
+}
+
+module.exports = {
+  assignVictoryLink
+};
