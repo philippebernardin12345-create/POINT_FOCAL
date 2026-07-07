1eace414709b5499dd3195a67eadff04dba5a926
philippebernardin12345-create <philippebernardin12345@gmail.com>
2026-07-07 07:26:35 +0100

    Delete src/modules/src/modules/victory/victory.routes.js

--- src/modules/src/modules/victory/victory.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const authMiddleware = require("../../middlewares/auth.middleware");
-const victoryController = require("./victory.controller");
-
-const router = express.Router();
-
-router.post("/assign", authMiddleware, victoryController.assignVictoryLink);
-
-module.exports = router;
