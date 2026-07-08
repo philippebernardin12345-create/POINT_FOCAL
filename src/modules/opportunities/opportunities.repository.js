ee4be95e1e7b0e94994b07d21f73b3318bde9421
philippebernardin12345-create <philippebernardin12345@gmail.com>
2026-07-08 01:47:06 +0100

    Update opportunities.repository.js

--- src/modules/opportunities/opportunities.repository.js
+++ src/modules/opportunities/opportunities.repository.js
@@ -1,1 +1,46 @@
 
+const db = require("../../config/database");
+
+async function findAllActive() {
+  const result = await db.query(
+    `
+    SELECT *
+    FROM opportunities
+    WHERE active = true
+    ORDER BY position ASC
+    `
+  );
+
+  return result.rows;
+}
+
+async function findAll() {
+  const result = await db.query(
+    `
+    SELECT *
+    FROM opportunities
+    ORDER BY position ASC
+    `
+  );
+
+  return result.rows;
+}
+
+async function findById(id) {
+  const result = await db.query(
+    `
+    SELECT *
+    FROM opportunities
+    WHERE id = $1
+    `,
+    [id]
+  );
+
+  return result.rows[0];
+}
+
+module.exports = {
+  findAllActive,
+  findAll,
+  findById
+};
