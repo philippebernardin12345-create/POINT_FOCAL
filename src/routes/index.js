50cd88321a18f3af453cb20d3a1d283c566f7a65
philippebernardin12345-create <philippebernardin12345@gmail.com>
2026-07-08 11:00:46 +0100

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const authRoutes = require("../modules/auth/auth.routes");
 const videoRoutes = require("../modules/video/video.routes");
 const victoryRoutes = require("../modules/victory/victory.routes");
+const victoryWorldRoutes = require("../modules/victory-world/victoryWorld.routes");
 const paymentsRoutes = require("../modules/payments/payments.routes");
 
 const router = express.Router();
@@ -17,6 +18,7 @@ router.get("/", (req, res) => {
 router.use("/auth", authRoutes);
 router.use("/video", videoRoutes);
 router.use("/victory-link", victoryRoutes);
+router.use("/victory-world", victoryWorldRoutes);
 router.use("/payment", paymentsRoutes);
 
 module.exports = router;
