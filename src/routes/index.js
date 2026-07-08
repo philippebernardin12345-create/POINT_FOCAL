58a9287b1a1690afb091eab83cb3e484ac1c3d35
philippebernardin12345-create <philippebernardin12345@gmail.com>
2026-07-08 09:49:01 +0100

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,6 @@ const authRoutes = require("../modules/auth/auth.routes");
 const videoRoutes = require("../modules/video/video.routes");
 const victoryRoutes = require("../modules/victory/victory.routes");
 const paymentsRoutes = require("../modules/payments/payments.routes");
-const opportunityRoutes = require("../modules/opportunities/opportunity.routes");
 
 const router = express.Router();
 
@@ -19,6 +18,5 @@ router.use("/auth", authRoutes);
 router.use("/video", videoRoutes);
 router.use("/victory-link", victoryRoutes);
 router.use("/payment", paymentsRoutes);
-router.use("/opportunities", opportunityRoutes);
 
 module.exports = router;
