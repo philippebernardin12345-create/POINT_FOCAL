ec933b8c7cadcaff987517651b00aa46ca119d1a
philippebernardin12345-create <philippebernardin12345@gmail.com>
2026-07-08 01:48:32 +0100

    Update opportunities.service.js

--- src/modules/opportunities/opportunities.service.js
+++ src/modules/opportunities/opportunities.service.js
@@ -1,1 +1,20 @@
 
+const opportunityRepository = require("./opportunity.repository");
+
+async function getActiveOpportunities() {
+  return await opportunityRepository.findAllActive();
+}
+
+async function getAllOpportunities() {
+  return await opportunityRepository.findAll();
+}
+
+async function getOpportunityById(id) {
+  return await opportunityRepository.findById(id);
+}
+
+module.exports = {
+  getActiveOpportunities,
+  getAllOpportunities,
+  getOpportunityById
+};
