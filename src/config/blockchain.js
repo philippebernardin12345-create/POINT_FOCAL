a9fc026df0245f2e67df4afa04831b173bdbd95c
philippebernardin12345-create <philippebernardin12345@gmail.com>
2026-07-09 15:34:36 +0100

    Update blockchain.js

--- src/config/blockchain.js
+++ src/config/blockchain.js
@@ -1,1 +1,22 @@
 
+const { Web3 } = require("web3");
+
+if (!process.env.BSC_RPC_URL) {
+  throw new Error("BSC_RPC_URL manquant dans les variables d'environnement.");
+}
+
+if (!process.env.USDT_BEP20_CONTRACT) {
+  throw new Error("USDT_BEP20_CONTRACT manquant dans les variables d'environnement.");
+}
+
+const web3 = new Web3(process.env.BSC_RPC_URL);
+
+const USDT_CONTRACT = process.env.USDT_BEP20_CONTRACT.toLowerCase();
+
+const ERC20_TRANSFER_TOPIC = web3.utils.sha3("Transfer(address,address,uint256)");
+
+module.exports = {
+  web3,
+  USDT_CONTRACT,
+  ERC20_TRANSFER_TOPIC
+};
