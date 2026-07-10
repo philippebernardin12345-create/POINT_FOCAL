479c4a5e9fdcb9a85e1f0325301b9a01e59b704c
philippebernardin12345-create <philippebernardin12345@gmail.com>
2026-07-10 03:11:29 +0100

    Update victory.service.js

--- src/modules/victory/victory.service.js
+++ src/modules/victory/victory.service.js
@@ -1,9 +1,13 @@
+const repository = require("./victory.repository");
+
 const DEFAULT_VICTORY_LINK = "https://victoryautomatic.com/user/register/okoningana";
 
 async function assignVictoryLink(userId) {
   if (!userId) {
     throw new Error("Utilisateur non authentifié.");
   }
+
+  await repository.markVictoryAssigned(userId);
 
   return {
     url: DEFAULT_VICTORY_LINK
