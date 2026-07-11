f1b9fe7671926f2676f2806498d4fd0fca88b086
philippebernardin12345-create <philippebernardin12345@gmail.com>
2026-07-11 23:05:09 +0100

    Update victory.routes.js

--- src/modules/victory/victory.routes.js
+++ src/modules/victory/victory.routes.js
@@ -1,9 +1,20 @@
 const express = require("express");
+
 const authMiddleware = require("../../middlewares/auth.middleware");
 const victoryController = require("./victory.controller");
 
 const router = express.Router();
 
-router.post("/assign", authMiddleware, victoryController.assignVictoryLink);
+router.post(
+  "/assign",
+  authMiddleware,
+  victoryController.assignVictoryLink
+);
+
+router.post(
+  "/reactivate",
+  authMiddleware,
+  victoryController.reactivateVictory
+);
 
 module.exports = router;
