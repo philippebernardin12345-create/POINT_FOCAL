9212cc65bab5be4e2a3dafc0ed8dfb104189af6c
philippebernardin12345-create <philippebernardin12345@gmail.com>
2026-07-13 09:30:20 +0100

    Update followme.routes.js

--- src/modules/followme/followme.routes.js
+++ src/modules/followme/followme.routes.js
@@ -1,1 +1,15 @@
 
+const express = require("express");
+
+const authMiddleware = require("../../middlewares/auth.middleware");
+const followmeController = require("./followme.controller");
+
+const router = express.Router();
+
+router.get(
+  "/:position",
+  authMiddleware,
+  followmeController.getSponsorLink
+);
+
+module.exports = router;
