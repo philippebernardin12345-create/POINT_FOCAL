b8755f382c44e1cfd10e507a09e7c350f00e6e38
philippebernardin12345-create <philippebernardin12345@gmail.com>
2026-07-21 08:48:49 +0100

    Update admin.routes.js

--- src/modules/admin/admin.routes.js
+++ src/modules/admin/admin.routes.js
@@ -1,1 +1,28 @@
 
+const express = require("express");
+
+const controller = require("./admin.controller");
+
+const router = express.Router();
+
+router.post(
+    "/login",
+    controller.login
+);
+
+router.get(
+    "/dashboard",
+    controller.dashboard
+);
+
+router.get(
+    "/users",
+    controller.users
+);
+
+router.get(
+    "/settings",
+    controller.settings
+);
+
+module.exports = router;
