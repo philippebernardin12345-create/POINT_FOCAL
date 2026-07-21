22740d12217e5a822ded4bcc46a5c84b6b44efb0
philippebernardin12345-create <philippebernardin12345@gmail.com>
2026-07-21 10:46:59 +0100

    Update admin.routes.js

--- src/modules/admin/admin.routes.js
+++ src/modules/admin/admin.routes.js
@@ -1,27 +1,37 @@
-
 const express = require("express");
 
-const controller = require("./admin.controller");
+const controller =
+    require("./admin.controller");
+
+const adminMiddleware =
+    require("../../middlewares/admin.middleware");
 
 const router = express.Router();
 
+
+// Connexion publique
 router.post(
     "/login",
     controller.login
 );
 
+
+// Routes protégées
 router.get(
     "/dashboard",
+    adminMiddleware,
     controller.dashboard
 );
 
 router.get(
     "/users",
+    adminMiddleware,
     controller.users
 );
 
 router.get(
     "/settings",
+    adminMiddleware,
     controller.settings
 );
 
