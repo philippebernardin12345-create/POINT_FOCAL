37c6054bbb61fff9e211d2b698b3526333ec5d20
philippebernardin12345-create <philippebernardin12345@gmail.com>
2026-07-21 09:36:29 +0100

    Update generate-admin-hash.js

--- generate-admin-hash.js
+++ generate-admin-hash.js
@@ -1,10 +1,11 @@
 const bcrypt = require("bcryptjs");
 
-const password = process.argv[2];
+const password =
+  process.env.ADMIN_TEMP_PASSWORD;
 
 if (!password) {
   console.log(
-    "Ajoute ton mot de passe après la commande."
+    "ADMIN_TEMP_PASSWORD est absente."
   );
 
   process.exit(1);
@@ -13,8 +14,29 @@ if (!password) {
 bcrypt
   .hash(password, 12)
   .then((hash) => {
+
+    console.log(
+      "================================"
+    );
+
+    console.log(
+      "ADMIN_PASSWORD_HASH :"
+    );
+
     console.log(hash);
+
+    console.log(
+      "================================"
+    );
+
   })
   .catch((error) => {
-    console.error(error.message);
+
+    console.error(
+      "Erreur :",
+      error.message
+    );
+
+    process.exit(1);
+
   });
