04b97b0344a66bbce00c0089dbb6630f276a95b7
philippebernardin12345-create <philippebernardin12345@gmail.com>
2026-07-22 21:51:48 +0100

    Create opportunities.engine.js

---
+++ src/modules/opportunities/opportunities.engine.js
@@ -0,0 +1,39 @@
+const repository = require("./opportunities.repository");
+
+async function validateFollowMeLink({
+  opportunityId,
+  personalLink,
+  realParentLink
+}) {
+  const parentAssignment =
+    await repository.findAssignmentByPersonalLink(
+      opportunityId,
+      realParentLink
+    );
+
+  if (!parentAssignment) {
+    throw new Error(
+      "Lien refusé : le parrain n'existe pas dans la base."
+    );
+  }
+
+  const existingLink =
+    await repository.findAssignmentByPersonalLink(
+      opportunityId,
+      personalLink
+    );
+
+  if (existingLink) {
+    throw new Error(
+      "Ce lien existe déjà."
+    );
+  }
+
+  return {
+    valid: true
+  };
+}
+
+module.exports = {
+  validateFollowMeLink
+};
