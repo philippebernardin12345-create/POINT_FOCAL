df423124bdba227c659f968750d39d84592dadfd
philippebernardin12345-create <philippebernardin12345@gmail.com>
2026-07-22 15:28:54 +0100

    Add routes for link, payment, and status

--- src/modules/victory-world/victory-world.routes.js
+++ src/modules/victory-world/victory-world.routes.js
@@ -1,1 +1,35 @@
+const express =
+  require("express");
 
+const authMiddleware =
+  require(
+    "../../middlewares/auth.middleware"
+  );
+
+const controller =
+  require(
+    "./victory-world.controller"
+  );
+
+const router =
+  express.Router();
+
+router.post(
+  "/link",
+  authMiddleware,
+  controller.saveLink
+);
+
+router.post(
+  "/payment",
+  authMiddleware,
+  controller.validatePayment
+);
+
+router.get(
+  "/status",
+  authMiddleware,
+  controller.getStatus
+);
+
+module.exports = router;
