1707e0446977d5ae8eb623b89fbfbf16099770f5
philippebernardin12345-create <philippebernardin12345@gmail.com>
2026-08-04 17:31:54 +0100

    Update opportunity.model.js

--- src/opportunities/opportunity.model.js
+++ src/opportunities/opportunity.model.js
@@ -1,1 +1,14 @@
 
+const opportunityModel = {
+  id: null,
+  name: "",
+  type: "configurable", // native | configurable
+  status: "inactive",   // active | inactive
+  priority: 1,
+  isEntry: false,
+  generatePointFocalLink: false,
+  registrationUrl: null,
+  dependsOn: null
+};
+
+module.exports = opportunityModel;
