f8a75a90674b7e3e5758f79bca1deec6c543ac54
philippebernardin12345-create <philippebernardin12345@gmail.com>
2026-08-04 17:35:11 +0100

    Update opportunity.controller.js

--- src/opportunities/opportunity.controller.js
+++ src/opportunities/opportunity.controller.js
@@ -1,1 +1,53 @@
 
+const opportunityService = require("./opportunity.service");
+const supabase = require("../../config/supabase");
+
+
+const getOpportunities = async (req, res) => {
+  try {
+
+    const opportunities =
+      await opportunityService.getAllOpportunities(supabase);
+
+    res.json({
+      success: true,
+      opportunities
+    });
+
+  } catch (error) {
+
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+
+  }
+};
+
+
+const getEntryOpportunity = async (req, res) => {
+  try {
+
+    const opportunity =
+      await opportunityService.getActiveEntryOpportunity(supabase);
+
+    res.json({
+      success: true,
+      opportunity
+    });
+
+  } catch (error) {
+
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+
+  }
+};
+
+
+module.exports = {
+  getOpportunities,
+  getEntryOpportunity
+};
