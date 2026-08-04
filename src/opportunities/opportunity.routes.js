715d60077f178738bf6d573d9cb509ae815b4afd
philippebernardin12345-create <philippebernardin12345@gmail.com>
2026-08-04 17:35:47 +0100

    Update opportunity.routes.js

--- src/opportunities/opportunity.routes.js
+++ src/opportunities/opportunity.routes.js
@@ -1,1 +1,20 @@
 
+const express = require("express");
+const router = express.Router();
+
+const controller = require("./opportunity.controller");
+
+
+router.get(
+  "/",
+  controller.getOpportunities
+);
+
+
+router.get(
+  "/entry",
+  controller.getEntryOpportunity
+);
+
+
+module.exports = router;
