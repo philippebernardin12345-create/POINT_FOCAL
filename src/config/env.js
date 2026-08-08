b389703ae4edc9836b68f694537deb2e3446bb3f
philippebernardin12345-create <philippebernardin12345@gmail.com>
2026-08-08 12:43:50 +0100

    Update env.js

--- src/config/env.js
+++ src/config/env.js
@@ -2,7 +2,18 @@ require("dotenv").config();
 
 module.exports = {
   port: process.env.PORT || 5000,
+
   databaseUrl: process.env.DATABASE_URL,
-  jwtSecret: process.env.JWT_SECRET || "CHANGE_ME_SECRET",
-  jwtExpiresIn: process.env.JWT_EXPIRES_IN || "7d"
+
+  jwtSecret:
+    process.env.JWT_SECRET || "CHANGE_ME_SECRET",
+
+  jwtExpiresIn:
+    process.env.JWT_EXPIRES_IN || "7d",
+
+  bscRpcUrl:
+    process.env.BSC_RPC_URL,
+
+  usdtBep20Contract:
+    process.env.USDT_BEP20_CONTRACT
 };
