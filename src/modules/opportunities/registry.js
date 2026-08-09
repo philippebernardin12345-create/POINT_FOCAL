4a260d31588a79e49aa024dc7db56ec75c0efb0e
philippebernardin12345-create <philippebernardin12345@gmail.com>
2026-08-09 14:27:58 +0100

    Create registry.js

---
+++ src/modules/opportunities/registry.js
@@ -0,0 +1,37 @@
+
+ 
+
+class OpportunityRegistry {
+  constructor() {
+    this.modules = new Map();
+  }
+
+  /**
+   * Enregistre un module d'opportunité
+   * @param {string} slug - ex: 'victory-automatic'
+   * @param {object} config - Configuration et routes du module
+   */
+  register(slug, moduleConfig) {
+    this.modules.set(slug, moduleConfig);
+    console.log(`[Registry] Module '${slug}' enregistré avec succès.`);
+  }
+
+  /**
+   * Récupère un module par son slug
+   */
+  get(slug) {
+    return this.modules.get(slug);
+  }
+
+  /**
+   * Liste tous les modules actifs
+   */
+  list() {
+    return Array.from(this.modules.values());
+  }
+}
+
+module.exports = new OpportunityRegistry();
+ 
+
+ 
