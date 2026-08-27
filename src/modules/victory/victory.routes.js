66cfd2c54eca2641aabe5f9722a034f6b3c5da8f
Philippe <philippebernardin12345@gmail.com>
2026-08-27 08:23:55 +0100

    fix(victory): use authenticate middleware

--- src/modules/victory/victory.routes.js
+++ src/modules/victory/victory.routes.js
@@ -1,24 +1,24 @@
 const express = require("express");
 
-const authMiddleware = require("../../middlewares/auth.middleware");
+const { authenticate } = require("../../middlewares/auth.middleware");
 const victoryController = require("./victory.controller");
 
 const router = express.Router();
 
 router.post(
   "/assign",
-  authMiddleware,
+  authenticate,
   victoryController.assignVictoryLink
 );
 
 router.post(
   "/reactivate",
-  authMiddleware,
+  authenticate,
   victoryController.reactivateVictory
 );
 router.post(
   "/personal-link",
-  authMiddleware,
+  authenticate,
   victoryController.saveVictoryPersonalLink
 );
 module.exports = router;
