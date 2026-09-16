072ee9baedeb691b4cf701b74da780ef4517c8a7
Philippe <philippebernardin12345@gmail.com>
2026-09-16 22:22:48 +0100

    diag(payment): log decoded USDT amount

--- src/modules/payments/payments.service.js
+++ src/modules/payments/payments.service.js
@@ -40,6 +40,7 @@ function decodeTransferLog(log) {
   const amount =
     Number(amountRaw) /
     Math.pow(10, USDT_DECIMALS);
+  console.log("[PAYMENT_DIAG] amountRaw =", amountRaw.toString(), "amount =", amount, "decimals =", USDT_DECIMALS);
 
   return {
     from: normalizeAddress(from),
